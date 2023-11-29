7324d6d9674b05f83174178b18b298542705e217
Mike <marquezini.mike@outlook.com>
2023-11-29 14:25:34 -0300

    feat:sparkles: createe model Orders

--- src/main/java/prosper/thelast/DTO/OrdersDTO.java
+++ src/main/java/prosper/thelast/DTO/OrdersDTO.java
@@ -1,2 +1,40 @@
-package prosper.thelast.DTO;public class OrdersDTO {
+package prosper.thelast.DTO;
+
+public class OrdersDTO {
+    private String id;
+    private String productName;
+    private String pointSales;
+    private int quantity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPointSales() {
+        return pointSales;
+    }
+
+    public void setPointSales(String pointSales) {
+        this.pointSales = pointSales;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/prosper/thelast/model/Orders.java
+++ src/main/java/prosper/thelast/model/Orders.java
@@ -1,2 +1,49 @@
-package prosper.thelast.model;public class Orders {
+package prosper.thelast.model;
+
+public class Orders {
+
+    private String id;
+    private String productName;
+    private String pointSales;
+    private int quantity;
+
+
+    @Override
+    public String toString(){
+        return "Orders{" +
+                "productName:'" + productName + '\'' +
+                ", quantity:'" + quantity + '\'' +
+                ", pointSales:" + pointSales + '}';
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPointSales() {
+        return pointSales;
+    }
+
+    public void setPointSales(String pointSales) {
+        this.pointSales = pointSales;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
