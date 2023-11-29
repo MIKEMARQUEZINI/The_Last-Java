78af68eead8bb9b42d1081b4bb90079cea0d2f8e
Mike <marquezini.mike@outlook.com>
2023-11-28 21:16:40 -0300

    init:tada: Initial configuration

---
+++ src/main/java/prosper/thelast/ThelastApplication.java
@@ -0,0 +1,13 @@
+package prosper.thelast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThelastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prosper/thelast/ThelastApplicationTests.java
@@ -0,0 +1,13 @@
+package prosper.thelast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThelastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
